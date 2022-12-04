3afc2c7bcf8292c675aa6ba567e7beae1d88a44b
Diogo Mesquita <doc.diogomesquita@hotmail.com>
2022-12-04 21:37:53 -0200

    first commit

---
+++ src/main/java/com/bookstore/restapibookstore/RestapiBookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.bookstore.restapibookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiBookstoreApplication.class, args);
+	}
+
+}
